154f263ef3d9dbbf154ffab615137a1dd58587d6
Jatin Chaudhary <chaudharyjatin8273@gmail.com>
2026-02-03 21:04:19 +0530

    [169] [Time Beats: 29.43%] [Memory Beats: 27.37%] - LeetPush

--- Majority-Element.cpp
+++ Majority-Element.cpp
@@ -1,21 +1,24 @@
 1class Solution {
 2public:
 3    int majorityElement(vector<int>& nums) {
-4        int n=nums.size();
-5        if(n==1)
-6        return nums[0];
-7        for(int i=0;i<n;i++){
-8        int flag=1;
-9            for(int j=i+1;j<n;j++){
-10                if(nums[i]==nums[j]){
-11                    flag++;
-12                }
-13                if(flag>(n/2))
+4        sort(nums.begin(),nums.end());
+5        int freq=1;
+6        int n=nums.size();
+7        if(n==1)
+8        return nums[0];
+9        for(int i=0;i<n-1;i++){
+10            if(nums[i+1]==nums[i]){
+11                freq++;
+12            }
+13            if(freq>(n/2)){
 14                return nums[i];
 15            }
-16            
-17
-18        }
-19        return {};
-20    }
-21};
+16            if(nums[i+1]!=nums[i])
+17            freq=1;
+18
+19
+20        }
+21        return {};
+22        
+23    }
+24};
