85d649bd580e1a2388b1ee77cad46ebd60bd032a
Jatin Chaudhary <chaudharyjatin8273@gmail.com>
2026-02-06 21:48:37 +0530

    [961] [Time Beats: 100.00%] [Memory Beats: 32.06%] - LeetPush

--- N-Repeated-Element-in-Size-2N-Array.cpp
+++ N-Repeated-Element-in-Size-2N-Array.cpp
@@ -1,13 +1,22 @@
 1class Solution {
 2public:
 3    int repeatedNTimes(vector<int>& nums) {
-4        int n=nums.size();
-5        for(int i=0;i<n;i++){
-6            for(int j=i+1;j<n;j++){
-7                if(nums[i]==nums[j])
-8                return nums[i];
-9            }
-10        }
-11        return {};
-12    }
-13};
+4     /* BRUTE FORCE   
+5     int n=nums.size();   
+6        for(int i=0;i<n;i++){
+7            for(int j=i+1;j<n;j++){
+8                if(nums[i]==nums[j])
+9                return nums[i];
+10            }
+11        }
+12        return {};*/
+13
+14         unordered_set<int> st;
+15
+16        for(int x : nums) {
+17            if(st.count(x)) return x;
+18            st.insert(x);
+19        }
+20        return -1;
+21    }
+22};
