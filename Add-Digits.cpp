4f35dd9ba5ede6391155ba2d17e8418f6f0ecc7c
Jatin Chaudhary <chaudharyjatin8273@gmail.com>
2026-02-16 17:48:31 +0530

    [258] [Time Beats: 100.00%] [Memory Beats: 44.25%] - LeetPush

--- Add-Digits.cpp
+++ Add-Digits.cpp
@@ -1,17 +1,8 @@
 1class Solution {
 2public:
 3    int addDigits(int num) {
-4        while (num >= 10) {   
-5            int sum = 0;
-6
-7            while (num > 0) {
-8                sum += num % 10;
-9                num /= 10;
-10            }
-11
-12            num = sum;
-13        }
-14        return num;
-15    }
-16};
-17
+4        if (num == 0) return 0;
+5        return 1 + (num - 1) % 9;
+6    }
+7};
+8
