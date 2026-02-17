96c4b0898f2455ad7cf93e84d8081c77e768c018
Jatin Chaudhary <chaudharyjatin8273@gmail.com>
2026-02-17 11:12:27 +0530

    [2974] [Time Beats: 100.00%] [Memory Beats: 95.22%] - LeetPush

--- Minimum-Number-Game.cpp
+++ Minimum-Number-Game.cpp
@@ -1,11 +1,11 @@
 1class Solution {
 2public:
 3    vector<int> numberGame(vector<int>& nums) {
-4        vector<int> arr(nums.begin(),nums.end());
-5        sort(arr.begin(),arr.end());
-6        for(int i=0;i<arr.size()-1;i+=2){
-7            swap(arr[i],arr[i+1]);
-8        }
-9        return arr;
+4         sort(nums.begin(),nums.end());
+5        for(int i=0;i<nums.size()-1;i+=2){
+6            swap(nums[i],nums[i+1]);
+7        }
+8        return nums;
+9    
 10    }
 11};
