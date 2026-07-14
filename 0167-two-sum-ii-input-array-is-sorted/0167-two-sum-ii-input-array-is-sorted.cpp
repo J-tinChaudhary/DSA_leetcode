52f5083ab21f8bc23137fa543f8d15e4013c2330
Jatin Chaudhary <chaudharyjatin8273@gmail.com>
2026-07-14 19:22:13 +0530

    0167-TWO-SUM-II-INPUT-ARRAY-IS-SORTED Stats: Time: 8 ms (1.59%), Space: 19.8 MB (33.72%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-
-        //BETTER APPROACH USE OF HASHMAP 
+        //BETTER APPROACH USE OF MAP
         unordered_map<int, int> mpp;
-        for (int i = 0; i < numbers.size(); i++) {
-            mpp[numbers[i]] = i;
-        }
+
         for (int i = 0; i < numbers.size(); i++) {
             int complement = target - numbers[i];
 
-            if (mpp.find(complement) != mpp.end() && mpp[complement] != i) {
-                return {i + 1, mpp[complement] + 1}; 
+            if (mpp.find(complement) != mpp.end()) {
+                return {mpp[complement] + 1, i + 1};
             }
+
+            mpp[numbers[i]] = i;
         }
 
         return {};
