c9ca9e7fbf9308f75a4a076f19f04486d98a3058
Jatin Chaudhary <chaudharyjatin8273@gmail.com>
2026-07-16 21:00:21 +0530

    0075-SORT-COLORS Stats: Time: 0 ms (100%), Space: 11.7 MB (11.7%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
+        //Dutch national flag algo
+        int low=0;
+        int mid=0;
+        int high=nums.size()-1;
+
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums[low],nums[mid]);
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==2){
+                swap(nums[mid],nums[high]);
+                high--;
+            }
+            else mid++;
+        }
         
-        int zero=0,one=0,two=0;
-            for(int i=0;i<nums.size();i++){
-                    if(nums[i]==0)zero++;
-                    else if(nums[i]==1)one++;
-                    else two++;
-            }
-            for(int i=0;i<zero;i++){
-                nums[i]=0;
-            }
-            for(int i=zero;i<one+zero;i++){
-                nums[i]=1;
-            }
-            for(int i=one+zero;i<nums.size();i++){
-                nums[i]=2;
-            }
-            
     }
 };
