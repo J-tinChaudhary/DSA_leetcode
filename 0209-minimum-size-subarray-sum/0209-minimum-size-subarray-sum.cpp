d3642b074e05bbba179131058f3715a7f06ae1c3
Jatin Chaudhary <chaudharyjatin8273@gmail.com>
2026-07-23 22:15:45 +0530

    0209-MINIMUM-SIZE-SUBARRAY-SUM Stats: Time: 0 ms (100%), Space: 42.1 MB (25.94%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -1,36 +1,26 @@
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
-        //BRUTE FORCE    
-    //     int n=nums.size();
-    //     int min_len = INT_MAX;
+        // OPTIMAL APPROACH WITH SLIDING WINDOW
+        // VARIABLE SIZE SLIDING WINDOW
+        int low = 0;
+        int high = 0;
+        int sum = 0;
+        int res = INT_MAX;
+        while (high < nums.size()) {
 
-    //     for(int i=0;i<n;i++){
-    //         int sum=0;
-    //         for(int j=i;j<n;j++){
-    //             sum+=nums[j];
-    //             if(sum>=target){
-    //                 min_len = min(min_len,j-i+1);
-    //                 break;
-    //             }
-    //         }
-    //     }
-    //     return (min_len== INT_MAX) ? 0 : min_len;
-    // }
-
-    int n=nums.size();
-    int i=0;
-    int j=0;
-    int minl=INT_MAX;
-    int sum=0;
-    while(j<n){
-        sum+=nums[j];
-        while(sum>=target){
-            minl=min(minl,j-i+1);
-            sum-=nums[i];
-            i++;
+            sum += nums[high];
+            while (sum >= target) {
+                int length = high - low + 1;
+                res = min(length, res);
+                sum -= nums[low];
+                low++;
+            }
+            high++;
         }
-        j++;
+        if (res ==INT_MAX)
+            return 0;
+        else
+            return res;
     }
-    return minl==INT_MAX ? 0:minl;  }
 };
